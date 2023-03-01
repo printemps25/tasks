26fa554a024864517c8a2f15a1e339a3ec55f0dc
printemps25 <sousakaren161@gmail.com>
2023-03-01 12:43:04 -0500

    Pushing again

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App(): JSX.Element {
                 UD CISC275 with React Hooks and TypeScript
             </header>
             <h1> Here is a Header! </h1>
-            <img src={Beagle} alt="Photo of a Beagle!" />
+            <img src={Beagle} alt="Photo of a Dog!" />
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Karen Sousa. Hello World.
@@ -37,5 +37,5 @@ function App(): JSX.Element {
         </div>
     );
 }
-
+   
 export default App;
