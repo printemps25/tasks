18961e54362c1be7d9f1a464f4598f17e33c3690
printemps25 <sousakaren161@gmail.com>
2023-02-28 22:32:53 -0500

    Testing for Task 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 import "./App.css";
+import Beagle from "./images/Beagle.png";
+import { Button } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import { Row } from "react-bootstrap";
+import { Col } from "react-bootstrap";
 
 function App(): JSX.Element {
     return (
@@ -7,11 +12,17 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
-            <h1> This is header text</h1>;
+            <h1> Here is a Header! </h1>
+            <img src={Beagle} alt="Photo of a Beagle!" />
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Karen Sousa. Hello World.
             </p>
+            <ul>
+                <li> Thing1 </li>
+                <li> Thing2 </li>
+                <li> Thing3 </li>
+            </ul>
         </div>
     );
 }
