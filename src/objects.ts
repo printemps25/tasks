92d1f097dfe5eee1949ccb2ff0c91fae323b92c7
printemps25 <sousakaren161@gmail.com>
2023-03-16 23:04:02 -0400

    More

--- src/objects.ts
+++ src/objects.ts
@@ -10,7 +10,16 @@ export function makeBlankQuestion(
     name: string,
     type: QuestionType
 ): Question {
-    return {};
+    return {
+        id: id,
+        name: name,
+        type: type,
+        body: "",
+        expected: "",
+        options: [],
+        points: 1,
+        published: false
+    };
 }
 
 /**
@@ -21,7 +30,10 @@ export function makeBlankQuestion(
  * HINT: Look up the `trim` and `toLowerCase` functions.
  */
 export function isCorrect(question: Question, answer: string): boolean {
-    return false;
+    const expectedAnswer = question.expected.trim().toLowerCase();
+    const providedAnswer = answer.trim().toLowerCase();
+
+    return expectedAnswer === providedAnswer;
 }
 
 /**
